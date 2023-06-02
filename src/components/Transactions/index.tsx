55919b569b7cc71d7b7a529a75bd5e8760bfed4e
Zach Byerley <zachbyerley@gmail.com>
2023-06-02 13:29:38 -0400

    fix(comp/Trans/index): only clears caches required. close #7

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -5,19 +5,18 @@ import { TransactionPane } from "./TransactionPane"
 import { SetTransactionApprovalFunction, TransactionsComponent } from "./types"
 
 export const Transactions: TransactionsComponent = ({ transactions }) => {
-  const { fetchWithoutCache, loading, clearCache } = useCustomFetch() //import clearCache or clearCachebyEndpoint?
+  const { fetchWithoutCache, loading, clearCacheByEndpoint } = useCustomFetch()
 
   const setTransactionApproval = useCallback<SetTransactionApprovalFunction>(
     async ({ transactionId, newValue }) => {
       //this works but would be nice to reset the cache of whichever page is not used, as well as by employee. Doesn't make sense to have to reload Mary's transactions because James changed his
-      //TODO: replace with clearCacheByEndpoint(paginatedTransactions or transactionsByEmployee)
-      clearCache()
+      clearCacheByEndpoint(["paginatedTransactions", "transactionsByEmployee"])
       await fetchWithoutCache<void, SetTransactionApprovalParams>("setTransactionApproval", {
         transactionId,
         value: newValue,
       })
     },
-    [fetchWithoutCache, clearCache]
+    [fetchWithoutCache, clearCacheByEndpoint]
   )
 
   if (transactions === null) {
